ef981a326524d7a375ba766dff989e303e0a1708
Yael Zamora <ajedrezkasparov@gmail.com>
2024-02-02 15:07:28 -0600

    Initial Commit

---
+++ FoodApp/FoodAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodAppApp.swift
+//  FoodApp
+//
+//  Created by Yael Javier Zamora Moreno on 02/02/24.
+//
+
+import SwiftUI
+
+@main
+struct FoodAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
